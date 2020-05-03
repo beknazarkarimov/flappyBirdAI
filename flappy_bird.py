04678f59e26ca0928c735550c60c404cc507fa52
beknazarkarimov <beknazar.bazarbekuulu@iaau.edu.kg>
2020-05-03 21:54:43 +0600

    Initial commit

---
+++ flappy_bird.py
@@ -0,0 +1,22 @@
+"""
+The classic game of flappy bird. Make with python
+and pygame. Features pixel perfect collision using masks :o
+Date Modified:  Jul 30, 2019
+Author: Tech With Tim
+Estimated Work Time: 5 hours (1 just for that damn collision)
+"""
+import pygame
+import random
+import os
+import time
+import neat
+import visualize
+import pickle
+pygame.font.init()  # init font
+
+WIN_WIDTH = 600
+WIN_HEIGHT = 800
+FLOOR = 730
+STAT_FONT = pygame.font.SysFont("comicsans", 50)
+END_FONT = pygame.font.SysFont("comicsans", 70)
+DRAW_LINES = False
